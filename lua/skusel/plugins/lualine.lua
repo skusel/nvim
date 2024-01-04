2d4309af386652d708cf8cae40752fe162d9b969
Larry Skuse <larry.skuse@gmail.com>
2024-01-04 13:53:02 -0500

    Add lualine plugin

---
+++ lua/skusel/plugins/lualine.lua
@@ -0,0 +1,32 @@
+local M = {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      icons_enabled = false,
+      section_separators = "",
+      component_separators = "",
+    },
+    sections = {
+      lualine_b = {
+        "branch",
+        {
+          "diagnostics",
+          sections = { "error", "warn" },
+          update_in_insert = true,
+          always_visible = true,
+        }
+      },
+      lualine_x = {
+        "encoding",
+        "filetype"
+      }
+    }
+  },
+  config = function(_, opts)
+    local lualine = require("lualine")
+    lualine.setup(opts)
+  end,
+}
+
+return M
